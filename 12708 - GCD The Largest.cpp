9da252bd33e7b9d90e47625064c77b4196f58069
dipuhasan <depu1994@gmail.com>
2015-07-04 09:43:09 +0600

    12708 - GCD The Largest.cpp

---
+++ 12708 - GCD The Largest.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int T;
+    long long n;
+    scanf("%d", &T);
+    while(T--){
+            scanf("%lld", &n);
+
+    printf("%lld\n", n>>1);
+    }
+
+
+    return 0;
+}
