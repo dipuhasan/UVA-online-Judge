444f1db9fbf20e15db9b3ce0c47ffeb9c5badce6
dipuhasan <depu1994@gmail.com>
2015-07-04 09:49:52 +0600

    Update 10922 - 2 the 9s.cpp

--- 10922 - 2 the 9s.cpp
+++ 10922 - 2 the 9s.cpp
@@ -1,43 +1,3 @@
-/*#include<cstdio>
-#include<cstring>
-using namespace std;
-
-int main()
-{
-    char a[101010];
-    while(gets(a)){
-        if(strcmp(a, "0")==0)
-            break;
-        int sum = 0;
-        int len = strlen(a);
-        for(int i = 0; i<len; i++){
-            sum +=a[i] - '0';
-        }
-        if(sum%9!=0){
-            printf("%s is not a multiple of 9.\n", a);
-
-        }
-
-     else{
-        int cnt = 1;
-        while(sum>9){
-            cnt++;
-            len = 0;
-        while(sum>0){
-         len +=sum%10;
-        sum =sum/10;
-        }
-        sum = len;
-        }
-     printf("%s is a multiple of 9 and has 9-degree %d.\n", a, cnt);
-
-    }
- }
-
-    return 0;
-}
-*/
-
 
 #include<cstdio>
 #include<iostream>
@@ -77,3 +37,42 @@ int main()
 
     return 0;
 }
+/*#include<cstdio>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char a[101010];
+    while(gets(a)){
+        if(strcmp(a, "0")==0)
+            break;
+        int sum = 0;
+        int len = strlen(a);
+        for(int i = 0; i<len; i++){
+            sum +=a[i] - '0';
+        }
+        if(sum%9!=0){
+            printf("%s is not a multiple of 9.\n", a);
+
+        }
+
+     else{
+        int cnt = 1;
+        while(sum>9){
+            cnt++;
+            len = 0;
+        while(sum>0){
+         len +=sum%10;
+        sum =sum/10;
+        }
+        sum = len;
+        }
+     printf("%s is a multiple of 9 and has 9-degree %d.\n", a, cnt);
+
+    }
+ }
+
+    return 0;
+}
+*/
